09939316a3bf792ad95d9a0c93dd9fa7b2c6a65f
Shayan Nasrabadi <williams.ca001@gmail.com>
2021-10-26 13:36:56 +0330

    E-3 Added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Discord = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 const config = require("./config.json");
-const bot = new Discord.Client({
-  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+const prefix = config.bot.prefix;
+const bot = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 // username = > testBot
@@ -18,14 +19,30 @@ bot.on("messageCreate", (message) => {
   const messageArry = message.content.split(" ");
   const cmd = messageArry[0].toLowerCase();
 
-  if (cmd.startsWith(`${config.bot.prefix}youtube`)) {
-    message.channel.send(
-      `${message.author} https://www.youtube.com/c/codewilliams`
-    );
-  } else if (cmd.startsWith(`${config.bot.prefix}insta`)) {
-    message.channel.send(
-      `${message.author} https://www.instagram.com/shayan._.williams`
-    );
+  if (cmd == `${prefix}embed`) {
+    const myEmbed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription("That's description")
+      .setTitle("That's title")
+      .setFooter(
+        "That's footer",
+        "https://yt3.ggpht.com/ytc/AKedOLRU-jr1r3QcPbiUn6Cu5_vReMAtf0VySaojH91veg=s900-c-k-c0x00ffffff-no-rj"
+      )
+      .setThumbnail(
+        "https://yt3.ggpht.com/ytc/AKedOLRU-jr1r3QcPbiUn6Cu5_vReMAtf0VySaojH91veg=s900-c-k-c0x00ffffff-no-rj"
+      )
+      .setImage(
+        "https://yt3.ggpht.com/ytc/AKedOLRU-jr1r3QcPbiUn6Cu5_vReMAtf0VySaojH91veg=s900-c-k-c0x00ffffff-no-rj"
+      )
+      .setAuthor(
+        "That's author",
+        "https://yt3.ggpht.com/ytc/AKedOLRU-jr1r3QcPbiUn6Cu5_vReMAtf0VySaojH91veg=s900-c-k-c0x00ffffff-no-rj",
+        "https://www.youtube.com/c/codewilliams"
+      )
+      .addField("Field Name", "Field Value")
+      .addField("Field 2", "Field Value 2", true);
+    const myEmbed2 = new MessageEmbed().setDescription("that's 2 embed");
+    message.channel.send({ content: "Salam", embeds: [myEmbed, myEmbed2] });
   }
 });
 
